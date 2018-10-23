8baf39b5b9b36ab977b9151c458df3eba9287b85
R00148064 Dominik Wiecek <dominik.wiecek@mycit.ie>
2018-10-23 23:26:37 +0100

    App

---
+++ app/src/main/java/com/example/domis/android_app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.domis.android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        Button registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.register_layout);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.login_layout);
+            }
+        });
+    }
+}
